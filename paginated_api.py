5520f11ca919dd735824028fdf97bf08396fcac1
Thanaphon <As-12@users.noreply.github.com>
2022-07-25 16:40:17 +0200

    Paginated_api should handle last page correctly

--- paginated_api.py
+++ paginated_api.py
@@ -13,14 +13,22 @@ def new_employee(id: int):
     return result
 
 def populate_mock_data():
-    for i in range(page_size):
+	page_count = data_size / page_size
+    for i in range(page_count - 1):
         mock_data.append({})
         mock_data[i]["employee_data"] = [new_employee(random.randint(0, 1000)) for i in range(page_size)]
         mock_data[i]["page_size"] = page_size
         mock_data[i]["page"] = i + 1
         mock_data[i]["page_count"] = page_count
         mock_data[i]["employee_count"] = page_count * page_size
-
+	last_page = page_count - 2
+	last_page_count = data_size % page_size
+	mock_data[last_page]["employee_data"] = [new_employee(random.randint(0, 1000)) for i in range(last_page_count)]
+    mock_data[last_page]["page_size"] = last_page_count
+    mock_data[last_page]["page"] = page_count
+    mock_data[last_page]["page_count"] = page_count
+    mock_data[last_page]["employee_count"] = page_count * page_size
+	
 @app.route('/')
 def default():
     return '', 204
